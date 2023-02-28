78ac9e9e3c83919051f8a321276745b623bbd7b9
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2023-02-28 22:56:52 +0300

    Update __init__.py

--- projectZ/__init__.py
+++ projectZ/__init__.py
@@ -19,9 +19,7 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2023 Xsarz'
 __version__ = '1.1.4.1.1'
 
-def init():
-	__newest__ = loads(get("https://pypi.org/pypi/projectZ.py/json").text)["info"]["version"]
-	if __version__ != __newest__:
-		s('cls || clear')
-		print(fore.ORANGE_1, f'{__title__} made by {__author__}\nPlease update the library. Your version: {__version__}  A new version:{__newest__}', fore.WHITE)
-init()
+__newest__ = loads(get("https://pypi.org/pypi/projectZ.py/json").text)["info"]["version"]
+if __version__ != __newest__:
+	s('cls || clear')
+	print(fore.ORANGE_1, f'{__title__} made by {__author__}\nPlease update the library. Your version: {__version__}  A new version:{__newest__}', fore.WHITE)
